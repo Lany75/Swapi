ca48ba12a01beca0a38aaf5e3d5e8b144af05b9b
Mélanie <mlanie.parry@gmail.com>
2020-04-28 14:50:35 +0200

    mise a jour de l'API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,48 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import ListPerso from '../ListPersos/ListPersos';
-import Infos from '../Infos/Infos';
-import Search from '../Search/Search';
+import ListPerso from "../ListPersos/ListPersos";
+import Infos from "../Infos/Infos";
+import Search from "../Search/Search";
 
-import './Home.css';
+import "./Home.css";
 
 class Home extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            listePersonnages: []
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      listePersonnages: [],
+    };
+  }
 
-    componentDidMount() {
-        this.getListPerso();
-    }
+  componentDidMount() {
+    this.getListPerso();
+  }
 
-    getListPerso() {
-        fetch ('https://swapi.co/api/people/')
-        .then (reponse => {
-            return reponse.json();
-        })
-        .then (data => {
-            //console.log(data.results);
-            this.setState({
-                listePersonnages: data.results
-            });
-            //console.log(this.state.listePersonnages);
-        })
-    }
+  getListPerso() {
+    fetch("https://swapi.dev/api/people/")
+      .then((reponse) => {
+        return reponse.json();
+      })
+      .then((data) => {
+        //console.log(data.results);
+        this.setState({
+          listePersonnages: data.results,
+        });
+        //console.log(this.state.listePersonnages);
+      });
+  }
 
-    render () {
-        return (
-            <div className='home'>
-                <Search />
-                <div className='persosInfos'>
-                    <ListPerso listePerso={this.state.listePersonnages} />
-                    <Infos listePerso={this.state.listePersonnages}/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="home">
+        <Search />
+        <div className="persosInfos">
+          <ListPerso listePerso={this.state.listePersonnages} />
+          <Infos listePerso={this.state.listePersonnages} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
